a3e8aa92095d44108fe21751bb6b44d357358ba3
elhassane47 <elhassane@fanprime.io>
2023-09-02 16:38:23 +0200

    more fields

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -6,14 +6,36 @@ from fastapi_users.db import (
     SQLAlchemyBaseUserTableUUID,
     SQLAlchemyUserDatabase,
 )
-
-from sqlalchemy import Column, Date
+from enum import Enum as Penum
+from sqlalchemy import Column, Date, Enum, String, ForeignKey, Integer
 
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from sqlalchemy.orm import relationship
+
+
+class GenderEnum(Penum):
+    MALE = "M"
+    FEMALE = "F"
+
+
+class City(Base):
+    __tablename__ = 'city'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True, nullable=False)
+    users = relationship("User", back_populates="city")
+
+    def __str__(self):
+        return self.name
 
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
     birthdate = Column(Date, nullable=True, index=True)
+    gender = Column(Enum(GenderEnum), nullable=True,)
+    mobile_number = Column(String(15), nullable=True)
+    city_id = Column(Integer, ForeignKey('city.id'), nullable=True)  # Reference 'city.id'
+    city = relationship("City", back_populates="users")
 
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
--- app/schemas/users.py
+++ app/schemas/users.py
@@ -3,14 +3,26 @@ import datetime
 from typing import Optional
 from fastapi_users import schemas
 
+from app.db.models import GenderEnum
+
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
-    birthdate: Optional[datetime.date]
+    birthdate: Optional[datetime.date] = None
+    mobile_number: Optional[str] = None
+    city_id: Optional[int] = None
+    gender: Optional[GenderEnum] = None
 
 
 class UserCreate(schemas.BaseUserCreate):
     birthdate: Optional[datetime.date] = None
+    mobile_number: Optional[str] = None
+    city_id: Optional[int] = None
+    gender: Optional[GenderEnum] = None
 
 
 class UserUpdate(schemas.BaseUserUpdate):
-    birthdate: Optional[datetime.date]
+    birthdate: Optional[datetime.date] = None
+    mobile_number: Optional[str] = None
+    city_id: Optional[int] = None
+    gender: Optional[GenderEnum] = None
+
